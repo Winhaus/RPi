5bd02b9d082efcfe37d274abd1c5857fb34de5ce
Winhaus <autohwh@hotmail.com>
2020-01-29 18:13:45 +0800

    available version

--- Ultrasonic/Ultrasonic.c
+++ Ultrasonic/Ultrasonic.c
@@ -1,54 +1,67 @@
 #include "wiringPi.h"
 #include "stdio.h"
+#include "stdlib.h"
 
 void EchoCac(void);
 
+
+int t, t_start,t_end;
+int Dist;
+	
+
 int main(int argc, char * argv[])
 {
-	wiringPiSetupGpio (void);
+	int i, cnt;
+	
+	wiringPiSetupGpio ();   // use BCM GPIO mapping
 	
 	pinMode (22, OUTPUT);
 	
 	pinMode (23, INPUT);
 	
-	if(argc != 2)
+    i = wiringPiISR (23, INT_EDGE_BOTH, &EchoCac);
+				
+	digitalWrite(22, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(22, LOW);	
+    printf("Ultrasonic Wave fire out!!!\n");	
+			
+
+	while(1)
 	{
-		exit(-1);
+	    cnt++;
 	}
-	else
-	{
-		wiringPiISR (23, INT_EDGE_RISING, EchoCac);
-		if((* argv[1]) == '1')
-		{
-			digitalWrite(22, HIGH);
-			delay(1);
-			digitalWrite(22, LOW);			
-		}			
-	}
-	
+
 }
 
 void EchoCac(void)
 {
-	int t, t_start,t_end;
-	int Dist;
 	
-	t_start = millis();
-	
-	printf("Target detected!!!\n")
-	
-	while(digitalRead (23) == HIGH)
+	if(digitalRead (23) == HIGH)
 	{
-      /* do nothing*/		
-	}
-	t_end = millis();
+	
+	t_start = micros();
+	
+	printf("Wave start：%d us\n",t_start);
+	
+	}	
+	
+	if(digitalRead (23) == LOW)
+	{
+	t_end = micros();
+	
+	printf("Echo got：%d us\n",t_end);
 	
 	t = t_end - t_start;
 	
-	Dist = (t*340)/2/1000;
+	Dist = (t*100*340)/2/1000000;
 	
-	printf("Distance: %d\n",Dist);	
-
+	printf("Target distance: %d cm\n",Dist);	
+	
+	exit(-1);
+	
+	}	
+	
  
 }
 
