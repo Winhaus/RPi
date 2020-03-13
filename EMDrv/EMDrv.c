1a660a8cc10f15b05ca7cfc575f77a99e662143e
Winhaus <autohwh@hotmail.com>
2020-03-13 21:31:10 +0800

    Electronic Motor Driver

---
+++ EMDrv/EMDrv.c
@@ -0,0 +1,41 @@
+#include "wiringPi.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+void EchoCac(void);
+
+
+int t, t_start,t_end;
+int Dist;
+	
+
+int main(int argc, char * argv[])
+{
+	int i, cnt;
+	
+	wiringPiSetupGpio ();   // use BCM GPIO mapping
+	
+	pinMode (26, OUTPUT);
+	
+	pinMode (20, INPUT);
+
+		
+
+			
+
+	while(1)
+	{
+	digitalWrite(26, HIGH);
+	digitalWrite(20, LOW);	
+	
+	delay(2000);
+	
+	digitalWrite(20, HIGH);
+	digitalWrite(26, LOW);	
+	}
+
+}
+
+
+
+
