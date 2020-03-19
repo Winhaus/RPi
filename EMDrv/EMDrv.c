77ce73fd77e1dd99b3d54144013bafe55e21ca08
Winhaus <autohwh@hotmail.com>
2020-03-19 22:06:09 +0800

    new version

--- EMDrv/EMDrv.c
+++ EMDrv/EMDrv.c
@@ -3,6 +3,12 @@
 #include "stdlib.h"
 
 
+void moveforwards();
+void movebackwards();
+void stop();
+void left();
+void right();
+
 int main(int argc, char * argv[])
 {
 	int i, cnt;
@@ -12,24 +18,79 @@ int main(int argc, char * argv[])
 	pinMode (26, OUTPUT);
 	
 	pinMode (20, OUTPUT);
+	
+	pinMode (19, OUTPUT);
+	
+	pinMode (16, OUTPUT);
 
 		
-
+if((* argv[1]) == '0')
+{
+stop();
+}
+else if((* argv[1]) == '1')
+	
+	{
+		moveforwards();
+	}
+else if((* argv[1]) == '2')
+{
+	movebackwards();
+}
+else if((* argv[1]) == '3')
+{
+	left();
+}
+else if((* argv[1]) == '4')
+{
+	right();
+}
 			
 
-	while(1)
-	{
-	digitalWrite(26, HIGH);
-	digitalWrite(20, LOW);	
-	
-	delay(2000);
-	
-	digitalWrite(20, HIGH);
-	digitalWrite(26, LOW);	
-	}
+
 
 }
 
+void moveforwards()
+{
+	digitalWrite(26, HIGH);
+	digitalWrite(20, LOW);	
+	digitalWrite(19, HIGH);
+	digitalWrite(16, LOW);	
+}
 
+void movebackwards()
+{
+
+	digitalWrite(20, HIGH);
+	digitalWrite(26, LOW);	
+	digitalWrite(16, HIGH);
+	digitalWrite(19, LOW);
+	
+}
+
+void stop()
+{
+	digitalWrite(26, LOW);
+	digitalWrite(20, LOW);	
+	digitalWrite(19, LOW);
+	digitalWrite(16, LOW);	
+}
+
+void left()
+{
+	digitalWrite(26, HIGH);
+	digitalWrite(20, LOW);	
+	digitalWrite(16, HIGH);
+	digitalWrite(19, LOW);	
+}
+
+void right()
+{
+	digitalWrite(20, HIGH);
+	digitalWrite(26, LOW);	
+	digitalWrite(19, HIGH);
+	digitalWrite(16, LOW);	
+}
 
 
